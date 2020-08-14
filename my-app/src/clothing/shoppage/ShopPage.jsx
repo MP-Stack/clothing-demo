a54884ee54369ec009665381eaf2cbf78e951caa
Manas <manaspasayat1@gmail.com>
2020-08-14 18:06:24 +0530

    implement withspinner HOC in shop page

--- my-app/src/clothing/shoppage/ShopPage.jsx
+++ my-app/src/clothing/shoppage/ShopPage.jsx
@@ -4,13 +4,31 @@ import {Route} from 'react-router-dom';
 import CollectionOverview from '../../clothing/collection-overview/collection.overview';
 import CollectionPage from '../../clothing/collection-page/collection-page';
 
-const ShopPage =({match})=>{
-    console.log(match)
+import WithSpinner from '../../clothing/with-spinner/with-spinner';
+
+const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+class ShopPage extends React.Component {
+  state={
+    loading:true
+  };
+
+    componentDidMount(){
+      this.setState({loading:false});
+    }
+
+    render(){
+      const {match} =this.props;
+      // const { loading } = this.State;
   return(
       <div className='shop-page'>
-          <Route exact path={`${match.path}`} component={CollectionOverview} />
-          <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+        < Route exact path={`${match.path}`} 
+          render={(props) =><CollectionOverviewWithSpinner isLoading={this.state.loading} {...props}/>}
+          />
+        <Route path={`${match.path}/:collectionId`} 
+          render ={(props) =><CollectionPageWithSpinner isLoading ={this.state.loading} {...props} />} />
        </div>
-  )}
+  )}}
     
-export default ShopPage;
+export default WithSpinner(ShopPage);
