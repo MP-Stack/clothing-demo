5534ce3f7ec7a51e86a526c06d9f06361a4d54c9
Manas <manaspasayat1@gmail.com>
2020-08-20 18:04:15 +0530

    create signup component and store data in firebase realtime-database with axios

--- my-app/src/clothing/Sign-In-and-Sign-Up/sign-in-and-sign-up.jsx
+++ my-app/src/clothing/Sign-In-and-Sign-Up/sign-in-and-sign-up.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './sign-in-and-sign-up.styles.scss';
 import SignIn from '../signin/SignIn';
-// import SignUp from '../signup/Signup';
+import SignUp from '../signup/Signup';
 const SignInAndSignUpPage =() =>(
   <div className='sign-in-and-sign-up'>
       <SignIn />
-      {/* <SignUp /> */}
+      <SignUp />
   </div>
 
 )
--- my-app/src/clothing/signup/Signup.jsx
+++ my-app/src/clothing/signup/Signup.jsx
@@ -1,98 +1,92 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Signup.scss';
-import FormInput from '../form-input/form-input';
-import CustomButton from '../../clothing/custom-button/custom.button';
-import {auth} from '../../clothing/firebase/firebase.utils';
+import FormInput from '../../clothing/form-input/form-input';
+import CustomButton from '../../clothing/custom-button/custom.button'; 
+import axios from 'axios';
 
-class SignUp extends Component {
-  state={
+
+
+class SignUp extends React.Component {
+  state = {
     displayName:'',
     email:'',
     password:'',
     confirmPassword:''
+  }
+
+  handleChange=(event)=>{
+    const {name,value}=event.target;
+    this.setState ({[name]:value});
   };
 
-  handleSubmit =async event=>
-  event.preventDefault();
-    // const {displayName, email , password,confirmPassword} = this.state;
-
-    // if(password !== confirmPassword){
-    //   alert("password don't match")
-    //   return;
-    // };
-
-    // try{ 
-    //   const {user} = await auth.createUserWithEmailAndPassword(email,password);
-     
-    //  await createUserProfileDocument(user,{displayName});
-    //  this.setState ({
-    //    displayName:'',
-    //     email:'',
-    //     password:'',
-    //    confirmPassword:''})
-
-    // }catch(error){
-    //   console.error(error);
-
-    // };
-  
-    handleChange=(event)=>{
-      const {name,value}=event.target;
-      this.setState ({[name]:value});
-    };
-
-  render() {
-   const {displayName, email , password,confirmPassword} = this.state;
-    return (
-       <div className='sign-up'>
-            <h2 className='title'>I don't have an account</h2>
-           <span>Sign up with your email and password.</span>
-
-         <form className='sign-up-form' onSubmit={this.handleSubmit}>
-
-           <FormInput type='text' 
-                      name='displayName' 
-                      value={displayName} 
-                      handleChange={this.handleChange} 
-                      label='Display Name' required/>
-           {/* <label>Email</label> */}
-
-           <FormInput 
-                type='email' 
-                name='email'  
-                value={email} 
-                handleChange={this.handleChange}
-                label='email'
-                required/>
-
-            <FormInput 
-                type='password' 
-                name='password'  
-                value={password} 
-                handleChange={this.handleChange}
-                label='password'
-                required/>
-
-            <FormInput 
-                type='password' 
-                name='confirmPassword'  
-                value={confirmPassword} 
-                handleChange={this.handleChange}
-                label='Confirm Password'
-                required/>
-
-           {/* <label>Password</label> */}
-      
-            <div className='buttons'>
-              <CustomButton onClick={this.SubmitRegister}>Sign Up</CustomButton>
-               
-             </div>
-        </form>
-  
-
-      </div>
-    )
+  handleSubmit = event =>{
+    event.preventDefault();
+    const User = {
+      displayName:this.state.displayName,
+      email:this.state.email,
+      password:this.state.password,
+      confirmPassword:this.state.confirmPassword
+    }
+    axios.post(`https://clothing-demo.firebaseio.com/marks.json`,{ User })
+    .then(res =>{
+      console.log (res);
+      console.log(res.data)
+    })
   }
-}
+ 
 
+  render(){
+     const {displayName, email , password,confirmPassword} = this.state;
+        return(
+            <div className='sign-up'>
+                 <h2 className='title'>I don't have an account</h2>
+                <span>Sign up with your email and password.</span>
+     
+              <form className='sign-up-form' onSubmit={this.handleSubmit}>
+     
+                <FormInput type='text' 
+                           name='displayName' 
+                           value={displayName} 
+                           handleChange={this.handleChange} 
+                           label='Display Name' required/>
+                {/* <label>Email</label> */}
+     
+                <FormInput 
+                     type='email' 
+                     name='email'  
+                     value={email} 
+                     handleChange={this.handleChange}
+                     label='email'
+                     required/>
+     
+                 <FormInput 
+                     type='password' 
+                     name='password'  
+                     value={password} 
+                     handleChange={this.handleChange}
+                     label='password'
+                     required/>
+     
+                 <FormInput 
+                     type='password' 
+                     name='confirmPassword'  
+                     value={confirmPassword} 
+                     handleChange={this.handleChange}
+                     label='Confirm Password'
+                     required/>
+     
+                {/* <label>Password</label> */}
+           
+                 <div className='buttons'>
+                   <CustomButton>Sign Up</CustomButton>
+                    
+                  </div>
+             </form>
+       
+     
+           </div>
+         )
+       }
+    
+  }
 export default SignUp;
